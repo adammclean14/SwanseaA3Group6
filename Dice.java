514722dd8d941c0c43ae793bee87092fd461af48
Fudz4 <fudz@live.co.uk>
2014-12-02 17:56:58 +0000

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,7 +1,7 @@
 COMMENTS STILL NEED DOING
 /**
- * @file - Dice.java
- * @author - F.Akkad
+ * @file	-Dice.java
+ * @author	-F.Akkad
  * @date	-2 Dec '14
  * 
  * Operates the dice function of the snakes and ladders game
@@ -13,14 +13,18 @@ import java.util.Random;
 
 public class Dice {
 	
-	/* the lowest boundary used in generating a random number */
+	/* the lowest boundary used in generating a random number (inclusive) */
 	final public static int LOW_DICEROLL = 1;
-	/* the highest boundary used in generating a random number */
-	final public static int HIGH_DICEROLL = 6;
+	/* the highest boundary used in generating a random number (exclusive) */
+	final public static int HIGH_DICEROLL = 7;
 	/* the number of times a number is generated before the final selection */
 	final public static int SHUFFLE = 11;
 	public static int m_Count = 0;
 	public static int m_SelectedNumber = 0;
+	
+	public static void main(String args[]) {
+		System.out.println(diceRoll());
+	}
 	
 	/**
 	 * Method to shuffle and then retrieve a randomly
@@ -37,7 +41,5 @@ public class Dice {
 		}
 		return m_SelectedNumber;
 	}
-
-}
 
 }
