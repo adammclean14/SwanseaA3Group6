014a12d5e15aca07ce182c6b50c657d3f263ac78
Jon Haddow <haddowjon@gmail.com>
2014-12-02 11:21:01 +0000

    Revert "Timer Class"
    This reverts commit 614b22699ae96ba985c85b7da98716718c54fa7d.

--- Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-	public static void main(String[] args) throws InterruptedException{
-		GameTimer timer = new GameTimer();
-		timer.startTime();
-		Thread.sleep(5000);
-		System.out.println(timer.getTime());
-	}
-}
