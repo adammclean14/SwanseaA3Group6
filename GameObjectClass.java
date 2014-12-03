d2d786a22dbb503b6adc075902d7c7ae1bf28a5a
Alan Woolley <alan@woolley.me>
2014-12-03 19:07:10 +0000

    added m_

--- GameObjectClass.java
+++ GameObjectClass.java
@@ -1,30 +1,30 @@
 
 public class GameObjectClass {
 
-  private int xCoord;
-  private int yCoord;
-  private int width;
-  private int height;
+  private int m_xCoord;
+  private int m_yCoord;
+  private int m_width;
+  private int m_height;
   
   //GETS X,Y COORDS, AND HEIGHT AND WIDTH.
 	
 	//Gets current X coordinate of the component's origin relative to the parent's upper left corner (measured in pixels)
 	public int getX() {
-		return xCoord;
+		return m_xCoord;
 	}
 	
 	//Gets current Y coordinate of the component's origin relative to the parent's upper left corner (measured in pixels)
 	public int getY() {
-		return yCoord;
+		return m_yCoord;
 	}	
 	
 	//Returns the current width of this component
 	public int getWidth(){
-		return width;
+		return m_width;
 	}
 	
 	//Returns the current height of this component
 	public int getHeight(){
-		return height;
+		return m_height;
 	}		
 }
