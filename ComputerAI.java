56fcbb6910e5d99551573a1efef1f97735fd57ac
Alan Woolley <alan@woolley.me>
2014-12-03 18:34:12 +0000

    Added new Contstructor

--- ComputerAI.java
+++ ComputerAI.java
@@ -9,10 +9,17 @@ public class ComputerAI extends Player{
 		m_delay = delay;
 	}
 
+	Player(String name, Color color, Boolean hasWon, int delay){
+		m_name = name;
+		m_pieceColor = color;
+		m_hasWon = hasWon;
+		m_delay = delay;
+	}
+
 	public void setDelay(int seconds){
 		m_delay = seconds;
 	}
-	
+
 	public int getDelay(){
 		return m_delay;
 	}
