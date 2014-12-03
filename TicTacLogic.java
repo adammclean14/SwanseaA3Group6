a7ecdf74f8bf07362e0168cd9db716d641f66b90
Alan Woolley <alan@woolley.me>
2014-12-03 19:05:59 +0000

    Added outline for TicTacLogic Class

---
+++ TicTacLogic.java
@@ -0,0 +1,18 @@
+public class TicTacLogic extends ComputerAi{
+
+	
+	private int[1] m_longestChain;
+
+
+	TicTacLogic{
+
+	}
+
+	public void getLength(){
+
+	}
+
+	public void setPosition(){
+
+	}
+}
