bf427ec7b04a7176ebccdced2ce1bcc6ef893723
JMurphy9 <j.t.murphy9@hotmail.co.uk>
2014-12-03 23:14:20 +0000

    Create SnakeLadderLogic.java

---
+++ SnakeLadderLogic.java
@@ -0,0 +1,23 @@
+public class SnakeLadderLogic extends ComputerAI {
+	
+	private int m_position;
+	private int m_getDice;
+	
+	public void compPosition() {
+		
+	}
+	
+	public void getDice() {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public Boolean won(){
+		return false;
+		
+			
+	}
+}
