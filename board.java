7e536e990d39408ab6dd83926775dc7cbe4a28c9
Jon Haddow <haddowjon@gmail.com>
2014-12-04 15:16:22 +0000

    Added Snake and ladder images

--- board.java
+++ board.java
@@ -1,7 +1,5 @@
 import java.awt.Color;
-import java.awt.Graphics;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class board {
 	/* execute application */
@@ -19,8 +17,8 @@ public class board {
 				/* display frame */
 				frame.setVisible( true );
 	} /* end main */
-	/** the width of the shapes panel */
+	/* the width of the shapes panel */
 	private final static int WIDTH = 1035;
-	/** the height of the shapes panel */
+	/* the height of the shapes panel */
 	private final static int HEIGHT = 2500;
 } /* end class LinesRectsOvals */
