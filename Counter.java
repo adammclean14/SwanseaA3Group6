0471b6775a083feadb9c84226af20f8b5f9d16d7
Jon Haddow <haddowjon@gmail.com>
2014-12-04 14:39:04 +0000

    Changed Counter

--- Counter.java
+++ Counter.java
@@ -1,78 +1,65 @@
 import java.awt.Color;
-import java.awt.DisplayMode;
-import java.awt.Image;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+/**
+ * @file Counter.java
+ * @author Adam Mclean
+ * Display colour and location of each counter 
+ */
 
-public class Counter /*extends Piece*/ {
-	
-	/*
-	 * Display colour and location of each piece (SnakeLadder)
-	 * movement animations for the following:
-	 * 		Up a ladder
-	 * 		Down a snake
-	 * 		Across the board
-	 * 		Diagonally across the board
-	 * 		Animation for winning the game (display message and counter blinks)
-	 */
-	
+public class Counter {
+
 	private Color color;
-	private String pieceImage;
-	private String counterColour;
-	
-	// Displays snakes image on the board based on location
-	
-	public void displaySnake(){
-		pieceImage = "snakeImage.jpeg";
-		JLabel snake = new JLabel();
-		Image image = new ImageIcon(this.getClass().getResource(pieceImage)).getImage();
-		snake.setIcon(new ImageIcon(image));
-	}
-	
-	// Displays ladders image on board based on location
-	public void displayLadder(){
-		pieceImage = "ladderImage.png";
-		JLabel ladder = new JLabel();
-		Image image = new ImageIcon(this.getClass().getResource(pieceImage)).getImage();
-		ladder.setIcon(new ImageIcon(image));
-	}
-	
-	// Animation for when counter moves up a ladder
-	public void upLadder(){
-		
-	}
-	
-	// Animation for when counter moves down a snake
-	public void downSnake(){
-		
-	}
-	
-	//Animation for the counter to move sideways
-	public void moveAcross(){
-		
-	}
-	
-	//Animation for the counter to move diagonally
-	public void moveDiagonal(){
-		
-	}
-	
-	// Set colour for counter 
-	public void setColor(Color color){
-		
-	}
-	
-	// Get method to get colour for counter
-	public Color getColor(){
-		return null;
-	}
-	
-	// Animation for winning the game (display message and counter blinks)
-	public void gameWon(){
-		JOptionPane.showMessageDialog(null, "Game over!");
-		//Code for counter blinking
+	private int locationX;
+	private int locationY;
+
+    /**
+     * Constructor counter which sets the counters location and color
+     * @param i
+     * @param j
+     */
+	public Counter(int i, int j){
+		setLocation(i,j);
 	}
 
+	/**
+	 * Set X and Y location
+	 * @param i
+	 * @param j
+	 */
+	private void setLocation(int i, int j) {
+		locationX = i;
+		locationY = j;
+	}
+	
+	/**
+	 * Get X location
+	 * @return X location
+	 */
+	public int getLocationX() {
+		return locationX;
+	}
+	
+	/**
+	 * Get Y location
+	 * @return Y location
+	 */
+	public int getLocationY() {
+		return locationY;
+	}
+
+	/**
+	 * Set colour for counter 
+	 * @param color of piece
+	 */
+	public void setColor(Color color){
+		this.color = color;
+	}
+
+	/**
+	 *  Get method to get colour for counter
+	 * @return color of piece
+	 */
+	public Color getColor(){
+		return color;
+	}
 }
