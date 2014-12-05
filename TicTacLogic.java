ca7773fb5c15e9a58f48f55d7a2c3b7906ced3f1
Jon Haddow <haddowjon@gmail.com>
2014-12-05 17:21:52 +0000

    Added even more comments

--- TicTacLogic.java
+++ TicTacLogic.java
@@ -1,5 +1,13 @@
 
-// @author Alan Alex Connor Riza Jon Adam Jack Fadl
+/**
+ * @file TicTacLogic.java
+ * @author Alan Alex Connor Riza Jon Adam Jack Fadl
+ * @date 5 Dec 14
+ * 
+ * This class is needed for the game to work. 
+ * @see TicTac.java and TicTacToe 
+ *
+ */
 public class TicTacLogic extends Player {
 
 }
