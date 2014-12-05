afa68e8d01dd356819f23e5a7d5e2ad04849e7f3
Jon Haddow <haddowjon@gmail.com>
2014-12-05 12:14:16 +0000

    Tidied BoardTile

--- BoardTile.java
+++ BoardTile.java
@@ -4,36 +4,32 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.util.Random;
-import java.util.Timer;
 import java.awt.*;
-
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
-
-
-
 import javax.swing.JPanel;
 import javax.swing.border.Border;
-
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
 
-
-public class BoardTile extends JPanel{
+public class BoardTile {
 	private JButton diceRollbtn;
 	private JFrame snakeLadderBoard;
 	static String imageLocation;
+	private JPanel boardPanel;
+	private JPanel menuPanel;
+	private JLabel diceLbl;
+	private JLabel timerTitle;
+	private JLabel timerFakeLbl;
+	private JLabel timerBox;
+	private JLabel playerLbl;
+	private JLabel turnLbl;
+	private JLabel playerBox;
 
-	public static void main (String[] args){
-
-	}
-	
-	
 	//Number of movers, number of players passed in
 	// produces a 10 x 10 grid for snakes and ladders
 	public void createSnakeLadderBoard(int numberOfMovers, int numberOfPlayers){
@@ -43,15 +39,112 @@ public class BoardTile extends JPanel{
 		snakeLadderBoard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		//try array of 1-100 with 10 rows incrementing properly
-		JPanel boardPanel = new JPanel();
+		boardPanel = new JPanel();
 		boardPanel.setLayout((new GridLayout(10, 10, 2, 2)));
 		boardPanel.setSize(750, 750);
 		boardPanel.setBounds(0, 0, 750, 750);
 
-
 		snakeLadderBoard.getContentPane().add(boardPanel);
 
+		drawBoardNumbers();
 
+		drawDice();
+
+		createSideMenu();
+
+		//Each square is roughly 75 pixels wide/high
+		snakeLadderBoard.setSize(1000,800);
+		snakeLadderBoard.setVisible(true);
+		snakeLadderBoard.setResizable(false);
+
+		snakeLadderBoard.add(new PaintSurface(), BorderLayout.CENTER);    
+		//snakeLadderBoard.add(new PaintCounter(), BorderLayout.CENTER);
+
+	}
+
+	private void createSideMenu() {
+		menuPanel = new JPanel();
+		//menuPanel.setLayout((new GridLayout(10, 10, 2, 2)));
+		menuPanel.setSize(150, 150);
+		menuPanel.setBounds(0, 0, 150, 300);
+		menuPanel.setLayout(null);
+		menuPanel.setBackground(Color.LIGHT_GRAY);
+		//added roll die btn   
+		diceRollbtn.setBounds(755, 350, 150, 65);
+		menuPanel.add(diceRollbtn);
+		//added dice animation/image
+		diceLbl = new JLabel();
+		diceLbl.setBounds(925, 350, 65, 65);
+		menuPanel.add(diceLbl);
+		//Timer Title Label
+		timerTitle = new JLabel("TIMER");
+		timerTitle.setBounds(815, 590, 150, 150);
+		timerTitle.setFont((new Font(timerTitle.getName(), Font.PLAIN, 35)));
+		menuPanel.add(timerTitle);
+		//Timer Number just for demonstration of placement...not functional
+		timerFakeLbl = new JLabel("0:23:15");
+		timerFakeLbl.setBounds(800, 635, 150, 150);
+		timerFakeLbl.setFont((new Font(timerFakeLbl.getName(), Font.PLAIN, 35)));
+		timerFakeLbl.setForeground(Color.BLUE);
+		menuPanel.add(timerFakeLbl);
+		//Label to make a border for the timer labels
+		timerBox = new JLabel("");
+		timerBox.setBounds(790,625,150,125);
+		Border timerBdr = BorderFactory.createLineBorder(Color.BLUE, 5);
+		timerBox.setBorder(timerBdr);
+		menuPanel.add(timerBox);
+		//Player Turn Lbl
+		playerLbl = new JLabel("YOUR TURN:");
+		playerLbl.setBounds(775, 150, 250, 150);
+		playerLbl.setFont((new Font(playerLbl.getName(), Font.PLAIN, 35)));
+		menuPanel.add(playerLbl);
+		//Player Turn Name Lbl
+		turnLbl = new JLabel("Jon");
+		turnLbl.setBounds(850, 175, 250, 200);
+		turnLbl.setFont((new Font(turnLbl.getName(), Font.PLAIN, 35)));
+		turnLbl.setForeground(Color.RED);
+		menuPanel.add(turnLbl);
+		//Label to make a border for the player labels
+		playerBox = new JLabel("");
+		playerBox.setBounds(765,175,225,150);
+		Border playerBdr = BorderFactory.createLineBorder(Color.DARK_GRAY, 5);
+		playerBox.setBorder(playerBdr);
+		menuPanel.add(playerBox);
+		snakeLadderBoard.getContentPane().add(menuPanel);
+	}
+
+
+	private void drawDice() {
+		String imageLocation = getRandNum();
+		final JLabel diceLbl = new JLabel("");
+		java.awt.Image image = new ImageIcon (this.getClass().getResource(imageLocation)).getImage();
+		diceLbl.setIcon(new ImageIcon(image));
+
+		final JButton diceRollbtn = new JButton("Roll Die");
+
+		diceRollbtn.addMouseListener(new MouseAdapter() {
+			int i = 0;
+			public void mousePressed(MouseEvent e){
+				if (i % 2 == 0){
+					diceRollbtn.setText("End Roll");
+					java.awt.Image gif = new ImageIcon (this.getClass().getResource("/diceBig.gif")).getImage();
+					diceLbl.setIcon(new ImageIcon(gif));
+					i = i + 1;
+				}
+				else{
+					diceRollbtn.setText("Start Roll");
+					String thing = getRandNum();
+					java.awt.Image image = new ImageIcon (this.getClass().getResource(thing)).getImage();
+					diceLbl.setIcon(new ImageIcon(image));
+					System.out.println(thing);	
+					i = i -1;
+				}
+			}
+		});
+	}
+
+
+	private void drawBoardNumbers() {
 		int x = 100;
 		int[][] numbers = new int[10][10];
 		boolean ascend = false;
@@ -114,114 +207,8 @@ public class BoardTile extends JPanel{
 				}
 			}
 		}   
-
-
-
-
-
-		String imageLocation = getRandNum();
-		final JLabel diceLbl = new JLabel("");
-		java.awt.Image image = new ImageIcon (this.getClass().getResource(imageLocation)).getImage();
-		diceLbl.setIcon(new ImageIcon(image));
-
-		final JButton diceRollbtn = new JButton("Roll Die");
-
-		diceRollbtn.addMouseListener(new MouseAdapter() {
-			int i = 0;
-			public void mousePressed(MouseEvent e){
-				if (i % 2 == 0){
-					diceRollbtn.setText("End Roll");
-					java.awt.Image gif = new ImageIcon (this.getClass().getResource("/diceBig.gif")).getImage();
-					diceLbl.setIcon(new ImageIcon(gif));
-					i = i + 1;
-				}
-				else{
-					diceRollbtn.setText("Start Roll");
-					String thing = getRandNum();
-					java.awt.Image image = new ImageIcon (this.getClass().getResource(thing)).getImage();
-					diceLbl.setIcon(new ImageIcon(image));
-					System.out.println(thing);	
-					i = i -1;
-				}}});
-
-
-		//Dice Stuff Panel
-		//The panel can't be moved to the right side so just move the components inside the panel
-		JPanel menuPanel = new JPanel();
-		//menuPanel.setLayout((new GridLayout(10, 10, 2, 2)));
-		menuPanel.setSize(150, 150);
-		menuPanel.setBounds(0, 0, 150, 300);
-		menuPanel.setLayout(null);
-		menuPanel.setBackground(Color.LIGHT_GRAY);
-		//added roll die btn   
-		diceRollbtn.setBounds(755, 350, 150, 65);
-		menuPanel.add(diceRollbtn);
-		//added dice animation/image
-		diceLbl.setBounds(925, 350, 65, 65);
-		menuPanel.add(diceLbl);
-
-		//Timer Title Label
-		JLabel timerTitle = new JLabel("TIMER");
-		timerTitle.setBounds(815, 590, 150, 150);
-		timerTitle.setFont((new Font(timerTitle.getName(), Font.PLAIN, 35)));
-		menuPanel.add(timerTitle);
-
-		//Timer Number just for demonstration of placement...not functional
-		JLabel timerFakeLbl = new JLabel("0:23:15");
-		timerFakeLbl.setBounds(800, 635, 150, 150);
-		timerFakeLbl.setFont((new Font(timerFakeLbl.getName(), Font.PLAIN, 35)));
-		timerFakeLbl.setForeground(Color.BLUE);
-		menuPanel.add(timerFakeLbl);
-
-		//Label to make a border for the timer labels
-		JLabel timerBox = new JLabel("");
-		timerBox.setBounds(790,625,150,125);
-		Border timerBdr = BorderFactory.createLineBorder(Color.BLUE, 5);
-		timerBox.setBorder(timerBdr);
-		menuPanel.add(timerBox);
-
-		//Player Turn Lbl
-		JLabel playerLbl = new JLabel("YOUR TURN:");
-		playerLbl.setBounds(775, 150, 250, 150);
-		playerLbl.setFont((new Font(playerLbl.getName(), Font.PLAIN, 35)));
-		menuPanel.add(playerLbl);
-
-		//Player Turn Name Lbl
-		JLabel turnLbl = new JLabel("Jon");
-		turnLbl.setBounds(850, 175, 250, 200);
-		turnLbl.setFont((new Font(turnLbl.getName(), Font.PLAIN, 35)));
-		turnLbl.setForeground(Color.RED);
-		menuPanel.add(turnLbl);
-
-		//Label to make a border for the player labels
-		JLabel playerBox = new JLabel("");
-		playerBox.setBounds(765,175,225,150);
-		Border playerBdr = BorderFactory.createLineBorder(Color.DARK_GRAY, 5);
-		playerBox.setBorder(playerBdr);
-		menuPanel.add(playerBox);
-
-		snakeLadderBoard.getContentPane().add(menuPanel);
-
-		//CREATING COUNTERS
-		//IDEALLY COUNTERS WILL BE JCOMPONENTS BECAUSE THEY ARE EASIER TO MOVE/TRACK AS SUCH
-		//HOWEVER LAYERING A JCOMP OVER THE GRID IS HARD.
-		//JLabel counter1 = new JLabel();
-		//java.awt.Image image3 = new ImageIcon (this.getClass().getResource("5dice.gif")).getImage();
-		//counter1.setIcon(new ImageIcon(image3));
-		//counter1.setBounds(735,250,225,150);
-		//menuPanel.add(counter1);
-
-
-		//Each square is roughly 75 pixels wide/high
-		snakeLadderBoard.setSize(1000,800);
-		snakeLadderBoard.setVisible(true);
-		snakeLadderBoard.setResizable(false);
-
-		snakeLadderBoard.add(new PaintSurface(), BorderLayout.CENTER);    
-		//snakeLadderBoard.add(new PaintCounter(), BorderLayout.CENTER);
-
-
 	}
+
 
 	public String getRandNum(){
 		int max = 6;
@@ -361,7 +348,7 @@ public class BoardTile extends JPanel{
 			g2d.drawImage(image2, trans8, this);
 			//------Done Drawing 4th SNAKE
 
-			
+
 			g2d.setColor(Color.RED);
 			g2d.fillOval(703,703,20,20);
 			g2d.setColor(Color.BLUE);
@@ -378,7 +365,7 @@ public class BoardTile extends JPanel{
 				//g2d.
 				i = i + 1;
 			}
-			}
+		}
 	}
 
 
@@ -386,7 +373,7 @@ public class BoardTile extends JPanel{
 	//+++++++
 	private class PaintCounter extends Canvas {
 		public PaintCounter() {
-		
+
 
 		}
 		private void paintBackground(Graphics2D g2){
@@ -405,7 +392,7 @@ public class BoardTile extends JPanel{
 			//------------------------------------
 			//PROBABLY SHOULD MAKE COUNTERS AS JLABELS WITH AN IMAGE
 			//MUCH EASIER TO MOVE AROUND AND CAN STORE AS VARIABLE
-			
+
 		}
 
 
