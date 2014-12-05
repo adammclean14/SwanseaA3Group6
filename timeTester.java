716541438e587da203be2246f4ee808aaab9ad21
Jon Haddow <haddowjon@gmail.com>
2014-12-05 17:02:01 +0000

    Timer tidied

--- timeTester.java
+++ timeTester.java
@@ -1,20 +1,26 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
 public class timeTester{
-    public static void main( String args[] ) {
-    	JFrame win = new JFrame( "Timer" );
-        win.addWindowListener(
-            new WindowAdapter() {
-                public void windowClosing( WindowEvent e ) {
-                    System.exit( 0 );
-                }
-            }
-        );
-        LabelTimer l = new LabelTimer();
-        Container content = win.getContentPane();
-        content.setLayout( new BorderLayout() );
-        content.add( "Center" , l );	
-        //JButton b = new JButton();
-        //content.add(b,BorderLayout.PAGE_START);
-        win.pack();
-        win.show();
-        l.timerStart();
+	public static void main( String args[] ) {
+		JFrame win = new JFrame( "Timer" );
+		win.addWindowListener(
+				new WindowAdapter() {
+					public void windowClosing( WindowEvent e ) {
+						System.exit( 0 );
+					}
+				}
+				);
+		LabelTimer l = new LabelTimer();
+		Container content = win.getContentPane();
+		content.setLayout( new BorderLayout() );
+		content.add( "Center" , l );	
+		win.pack();
+		win.show();
+		l.timerStart();
+	}
 }
