5e18121e8efa024314030e0f78c36b90cf84909a
Alan Woolley <alan@woolley.me>
2014-12-05 17:36:24 +0000

    Added Official owner the quote

--- TicTacLogic.java
+++ TicTacLogic.java
@@ -13,7 +13,7 @@ public class TicTacLogic extends Player {
 
 	/**
 	* “That's the thing about people who think they hate computers. What they really hate is lousy programmers.” 
-	* - Larry Niven
+	* - Riza Gulsen
 	*/
 
 }
