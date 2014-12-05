ed91fba708e2ef41ff9516323b4a30036e52bb62
Jon Haddow <haddowjon@gmail.com>
2014-12-05 12:15:31 +0000

    Changed snakeLadder class

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,50 +1,18 @@
 /**
- * @file	-SnakeLadder.java
- * @author	-F.Akkad
- * @date	-2 Dec '14
+ * @file	SnakeLadder.java
+ * @author	F.Akkad
+ * @date	2 Dec '1
  * 
  * 
  */
-package gamesproject;
-import java.util.Random;
+import java.awt.Color;
 
 public class SnakeLadder {
-	
-	public static int m_POneCorner;
-	public static int m_PTwoCorner;
-	public static int m_PThreeCorner;
-	public static int m_PFourCorner;
-	public static int m_MoveNumber;
-	public static int m_SetTail;
-	public static int m_SetHead;
-	
 
-	public void getSnakes() {
+	public SnakeLadder(int numberOfMovers, int numberOfPlayers, String[] playerNames, Color[] playerColors){
+		BoardTile board = new BoardTile();
+		board.createSnakeLadderBoard(numberOfMovers, numberOfPlayers);
+		
 		
 	}
-	
-	public void getLadders() {
-		
-	}
-	
-	public void setCorner() {
-		
-	}
-	
-	public static int getMoves() {
-		Dice dice = new Dice();
-		return Dice.diceRoll();
-	}
-	
-	public void move() {
-		m_MoveNumber = getMoves();
-		
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(getMoves());
-
-	}
-
 }
